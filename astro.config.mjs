21432b6fe7ca89d21780890442f8da18620dc5d8
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-04-29 15:59:05 -0300

    feat: add SSR in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   image: {
-    service: passthroughImageService(),
+    service: passthroughImageService()
   },
+  output: "server",
+  adapter: vercel()
 });
---
+++ src/pages/api/hello.ts
@@ -0,0 +1,17 @@
+export async function GET() {
+  return new Response(JSON.stringify({ msg: "Hello World!" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export async function POST() {
+  return new Response(JSON.stringify({ msg: "Hello World!" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
